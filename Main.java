ba9f50e6424774369efcbf901bc42177d7e9bb2c
Jakub Wnęk <127328405+Wneq1@users.noreply.github.com>
2023-03-22 15:25:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    float a =2;
+    float b=13;
+
+    float suma =a+b;
+    float roznica =a-b;
+    float iloczyn =a*b;
+    float iloraz =a/b;
+
+
+      System.out.println("a ="+ a);
+      System.out.println("b ="+ b);
+    
+      System.out.println("suma ="+ suma);
+      System.out.println("roznica ="+ roznica);
+      System.out.println("iloczyn ="+ iloczyn);
+      System.out.println("iloraz ="+ iloraz);
+  }
+}
